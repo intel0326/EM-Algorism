4e6624539817dc81f0068f0bd51359484ae0759a
intel0326 <is0269rx@ed.ritsumei.ac.jp>
2018-10-30 18:12:25 +0900

    first commit

---
+++ em.py
@@ -0,0 +1,147 @@
+#coding: utf-8
+import os
+import numpy as np
+import numpy.random as rd
+import scipy as sp
+from scipy import stats as st
+from collections import Counter
+
+import matplotlib
+from matplotlib import font_manager
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib import rc
+
+
+# ======================================
+# Parameters
+K = 3
+n = 301
+xx = np.linspace(-4, 7, n)
+
+mu = [-2, 0, 2]
+sigma = [0.5, 0.7, 1.5]
+pi = [0.2, 0.3, 0.5]
+
+# Density function
+pdfs = np.zeros((n, K))
+for k in range(K):
+    pdfs[:, k] = pi[k]*st.norm.pdf(xx, loc=mu[k], scale=sigma[k])
+
+# ======================================
+# 山下先生のParameters
+cluster = 2
+#行数10000を指定
+N = 10000
+#変数、データの次元数
+D = 2
+
+#ファイル読み込み, 2列10000行で取得
+data = np.loadtxt("data1000.txt", delimiter=',', unpack=True)
+
+#print(data.shape)
+#print(data)
+#print(type(data))
+
+#plt.scatter(data[0,:], data[1,:], s=30, c='gray', alpha=0.5, marker="+")
+plt.scatter(data[0,:], data[1,:], c='gray', alpha=0.5, marker="+")
+plt.show()
+
+'''
+# =======================================
+# Visualization
+#plt.figure(figsize=(14, 6))
+for k in range(K):
+    plt.plot(xx, pdfs[:, k])
+plt.title("pdfs")
+plt.show()
+
+plt.figure(figsize=(14, 6))
+plt.stackplot(xx, pdfs[:, 0], pdfs[:, 1], pdfs[:, 2])
+plt.title("stacked")
+plt.show()
+'''
+
+'''
+#イテレーション100回
+for nframe in range(100):
+    global mu, sigma, pi
+    print('nframe:', nframe)
+    plt.clf()
+
+    if nframe <= 3:
+        print('initial state')
+        plt.scatter(data[:,0], data[:,1], s=30, c='gray', alpha=0.5, marker="+")
+        for i in range(3):
+            plt.scatter([mu[i, 0]], [mu[i, 1]], c=c[i], marker='o', edgecolors='k', linewidths=1)
+        print_gmm_contour(mu, sigma, pi, K)
+        plt.title('initial state')
+        return
+
+    # E step ========================================================================
+    # calculate responsibility(負担率)
+    likelihood = calc_likelihood(data, mu, sigma, pi, K)
+    gamma = (likelihood.T/np.sum(likelihood, axis=1)).T
+    N_k = [np.sum(gamma[:,k]) for k in range(K)]
+
+    # M step ========================================================================
+
+    # caluculate pi
+    pi =  N_k/N
+
+    # calculate mu
+    tmp_mu = np.zeros((K, D))
+
+    for k in range(K):
+        for i in range(len(data)):
+            tmp_mu[k] += gamma[i, k]*data[i]
+        tmp_mu[k] = tmp_mu[k]/N_k[k]
+    mu_prev = mu.copy()
+    mu = tmp_mu.copy()
+
+    # calculate sigma
+    tmp_sigma = np.zeros((K, D, D))
+
+    for k in range(K):
+        tmp_sigma[k] = np.zeros((D, D))
+        for i in range(N):
+            tmp = np.asanyarray(data[i]-mu[k])[:,np.newaxis]
+            tmp_sigma[k] += gamma[i, k]*np.dot(tmp, tmp.T)
+        tmp_sigma[k] = tmp_sigma[k]/N_k[k]
+
+    sigma = tmp_sigma.copy()
+
+    # calculate likelihood
+    prev_likelihood = likelihood
+    likelihood = calc_likelihood(data, mu, sigma, pi, K)
+
+    prev_sum_log_likelihood = np.sum(np.log(prev_likelihood))
+    sum_log_likelihood = np.sum(np.log(likelihood))
+    diff = prev_sum_log_likelihood - sum_log_likelihood
+
+    print('sum of log likelihood:', sum_log_likelihood)
+    print('diff:', diff)
+
+    print('pi:', pi)
+    print('mu:', mu)
+    print('sigma:', sigma)
+
+    # visualize
+    for i in range(N):
+        plt.scatter(data[i,0], data[i,1], s=30, c=gamma[i], alpha=0.5, marker="+")
+
+    for i in range(K):
+        ax = plt.axes()
+        ax.arrow(mu_prev[i, 0], mu_prev[i, 1], mu[i, 0]-mu_prev[i, 0], mu[i, 1]-mu_prev[i, 1],
+                  lw=0.8, head_width=0.02, head_length=0.02, fc='k', ec='k')
+        plt.scatter([mu_prev[i, 0]], [mu_prev[i, 1]], c=c[i], marker='o', alpha=0.8)
+        plt.scatter([mu[i, 0]], [mu[i, 1]], c=c[i], marker='o', edgecolors='k', linewidths=1)
+    plt.title("step:{}".format(nframe))
+
+    print_gmm_contour(mu, sigma, pi, K)
+
+    if np.abs(diff) < 0.0001:
+        plt.title('likelihood is converged.')
+    else:
+        plt.title("iter:{}".format(nframe-3))
+'''
