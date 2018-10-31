6c0f28ec30b30377f908c79fc141620d2bf9373e
intel <is0269rx@ed.ritsumei.ac.jp>
2018-10-31 18:38:56 +0900

    10.31 期待値計算の関数を作製

--- em.py
+++ em.py
@@ -13,20 +13,12 @@ import matplotlib.cm as cm
 from matplotlib import rc
 
 
-# ======================================
-# Parameters
-K = 3
-n = 301
-xx = np.linspace(-4, 7, n)
+def calc_likelihood(data, mean, sigma, weight, D):
+    likelihood = np.zeros((N, 2))
+    for d in range(D):
+        likelihood[:, d] = [weight[d]*st.multivariate_normal.pdf(i, mean[d], sigma[d]) for i in data]
+    return likelihood
 
-mu = [-2, 0, 2]
-sigma = [0.5, 0.7, 1.5]
-pi = [0.2, 0.3, 0.5]
-
-# Density function
-pdfs = np.zeros((n, K))
-for k in range(K):
-    pdfs[:, k] = pi[k]*st.norm.pdf(xx, loc=mu[k], scale=sigma[k])
 
 # ======================================
 # 山下先生のParameters
@@ -36,68 +28,65 @@ N = 10000
 #変数、データの次元数
 D = 2
 
-
 #ファイル読み込み, 2列10000行で取得
 data = np.loadtxt("data1000.txt", delimiter=',', unpack=True)
 
-#print(data.shape)
-#print(data)
-#print(type(data))
+'''
+print(data.shape)
+print(data)
+print(type(data))
 
 plt.scatter(data[0,:], data[1,:], c='gray', alpha=0.5, marker="+")
 plt.show()
+'''
 
+# ======================================
 #イテレーション100回
-for nframe in range(100):
-    global mu, sigma, pi
-    print('nframe:', nframe)
-    plt.clf()
+for iteration in range(100):
+    
+    #パラメータの初期化
+    global weight
+    global mean
+    global sigma
+    print('iteration:', iteration)
 
-    if nframe <= 3:
-        print('initial state')
-        plt.scatter(data[:,0], data[:,1], s=30, c='gray', alpha=0.5, marker="+")
-        for i in range(3):
-            plt.scatter([mu[i, 0]], [mu[i, 1]], c=c[i], marker='o', edgecolors='k', linewidths=1)
-        print_gmm_contour(mu, sigma, pi, K)
-        plt.title('initial state')
-        return
 
     # E step ========================================================================
     # calculate responsibility(負担率)
-    likelihood = calc_likelihood(data, mu, sigma, pi, K)
+    likelihood = calc_likelihood(data, mean, sigma, weight, D)
     gamma = (likelihood.T/np.sum(likelihood, axis=1)).T
-    N_k = [np.sum(gamma[:,k]) for k in range(K)]
+    N_k = [np.sum(gamma[:,d]) for d in range(D)]
 
     # M step ========================================================================
 
-    # caluculate pi
-    pi =  N_k/N
+    # caluculate weight
+    weight =  N_k/N
 
-    # calculate mu
-    tmp_mu = np.zeros((K, D))
+    # calculate mean
+    tmp_mu = np.zeros((D, D))
 
-    for k in range(K):
+    for d in range(D):
         for i in range(len(data)):
-            tmp_mu[k] += gamma[i, k]*data[i]
-        tmp_mu[k] = tmp_mu[k]/N_k[k]
-    mu_prev = mu.copy()
-    mu = tmp_mu.copy()
+            tmp_mu[d] += gamma[i, d]*data[i]
+        tmp_mu[d] = tmp_mu[d]/N_k[d]
+    mu_prev = mean.copy()
+    mean = tmp_mu.copy()
 
     # calculate sigma
-    tmp_sigma = np.zeros((K, D, D))
+    tmp_sigma = np.zeros((D, D, D))
 
-    for k in range(K):
-        tmp_sigma[k] = np.zeros((D, D))
+    for d in range(D):
+        tmp_sigma[d] = np.zeros((D, D))
         for i in range(N):
-            tmp = np.asanyarray(data[i]-mu[k])[:,np.newaxis]
-            tmp_sigma[k] += gamma[i, k]*np.dot(tmp, tmp.T)
-        tmp_sigma[k] = tmp_sigma[k]/N_k[k]
+            tmp = np.asanyarray(data[i]-mean[d])[:,np.newaxis]
+            tmp_sigma[d] += gamma[i, d]*np.dot(tmp, tmp.T)
+        tmp_sigma[d] = tmp_sigma[d]/N_k[d]
 
     sigma = tmp_sigma.copy()
 
     # calculate likelihood
     prev_likelihood = likelihood
-    likelihood = calc_likelihood(data, mu, sigma, pi, K)
+    likelihood = calc_likelihood(data, mean, sigma, weight, D)
 
     prev_sum_log_likelihood = np.sum(np.log(prev_likelihood))
     sum_log_likelihood = np.sum(np.log(likelihood))
@@ -106,25 +95,25 @@ for nframe in range(100):
     print('sum of log likelihood:', sum_log_likelihood)
     print('diff:', diff)
 
-    print('pi:', pi)
-    print('mu:', mu)
+    print('weight:', weight)
+    print('mean:', mean)
     print('sigma:', sigma)
 
     # visualize
     for i in range(N):
         plt.scatter(data[i,0], data[i,1], s=30, c=gamma[i], alpha=0.5, marker="+")
 
-    for i in range(K):
+    for i in range(D):
         ax = plt.axes()
-        ax.arrow(mu_prev[i, 0], mu_prev[i, 1], mu[i, 0]-mu_prev[i, 0], mu[i, 1]-mu_prev[i, 1],
+        ax.arrow(mu_prev[i, 0], mu_prev[i, 1], mean[i, 0]-mu_prev[i, 0], mean[i, 1]-mu_prev[i, 1],
                   lw=0.8, head_width=0.02, head_length=0.02, fc='k', ec='k')
         plt.scatter([mu_prev[i, 0]], [mu_prev[i, 1]], c=c[i], marker='o', alpha=0.8)
-        plt.scatter([mu[i, 0]], [mu[i, 1]], c=c[i], marker='o', edgecolors='k', linewidths=1)
-    plt.title("step:{}".format(nframe))
+        plt.scatter([mean[i, 0]], [mean[i, 1]], c=c[i], marker='o', edgecolors='k', linewidths=1)
+    plt.title("step:{}".format(iteration))
 
-    print_gmm_contour(mu, sigma, pi, K)
+    #print_gmm_contour(mean, sigma, weight, D)
 
     if np.abs(diff) < 0.0001:
         plt.title('likelihood is converged.')
     else:
-        plt.title("iter:{}".format(nframe-3))
+        plt.title("iter:{}".format(iteration-3))
