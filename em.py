c7f43948865e07f32f8f248251b0ddf607a45921
intel <is0269rx@ed.ritsumei.ac.jp>
2018-10-31 22:39:01 +0900

    2次元の混合正規分布の形成を完成
    2次元の混合正規分布の形成の完成
    平均や共分散，重みの初期化が完成
    
    dataの可視化にtarans_dataを新たに用意して，行と列を入れ替えてる
    
    Eステップ2コード目に何らかの問題あり

--- em.py
+++ em.py
@@ -13,30 +13,66 @@ import matplotlib.cm as cm
 from matplotlib import rc
 
 
-def calc_likelihood(data, mean, sigma, weight, D):
-    likelihood = np.zeros((N, 2))
+def MixtureGaussian(data, mean, sigma, weight, D, cluster):
+    PDF = np.zeros((N, cluster))
     for d in range(D):
-        likelihood[:, d] = [weight[d]*st.multivariate_normal.pdf(i, mean[d], sigma[d]) for i in data]
-    return likelihood
+        #混合正規分布
+        PDF[:, d] = [weight[d]*st.multivariate_normal.pdf(i, mean[d], sigma[d]) for i in data]
+    return PDF
 
 
 # ======================================
-# 山下先生のParameters
+# Parameters
+# 混合数
 cluster = 2
-#行数10000を指定
+# 行数10000を指定
 N = 10000
-#変数、データの次元数
+# 変数、データの次元数
 D = 2
+# パラメータの初期化
+global weight
+global mean
+global sigma
+# カラー
+c = ['r', 'g', 'b']
+
+
+#共分散
+sigma = np.asanyarray(
+    [ [[0.1,  0],[ 0, 0.1]],
+        [[0.1,  0],[ 0, 0.1]] ])
+print('sigma: ', sigma.shape)
+print('1分布に対する共分散sigma[d]', sigma[0].shape)
+
+
+#重み
+weight = np.zeros(cluster)
+#制約条件(w[0]+w[1]=1)に従って各正規分布の初期重みを設定
+for k in range(cluster):
+    if k == cluster - 1:
+        weight[k] = 1 - np.sum(weight)
+    else:
+        weight[k] = 1 / cluster
+print('initial weight: ', weight.shape)
+
 
 #ファイル読み込み, 2列10000行で取得
-data = np.loadtxt("data1000.txt", delimiter=',', unpack=True)
+#data = np.loadtxt("data1000.txt", delimiter=',', unpack=True)
+data = np.loadtxt("data1000.txt", delimiter=',')
+print(data.shape)
+#(10000, 2)
+#print(data)
+#print(type(data))
+
+#平均
+max_x, min_x = np.max(data[:,0]), np.min(data[:,0])
+max_y, min_y = np.max(data[:,1]), np.min(data[:,1])
+mean = np.c_[rd.uniform(low=min_x, high=max_x, size=cluster), rd.uniform(low=min_y, high=max_y, size=cluster) ]
+print('mean: ', mean.shape)
 
 '''
-print(data.shape)
-print(data)
-print(type(data))
-
-plt.scatter(data[0,:], data[1,:], c='gray', alpha=0.5, marker="+")
+trans_data = data.transpose()
+plt.scatter(trans_data[0, :], trans_data[1, :], c='gray', alpha=0.5, marker="+")
 plt.show()
 '''
 
@@ -44,31 +80,33 @@ plt.show()
 #イテレーション100回
 for iteration in range(100):
     
-    #パラメータの初期化
-    global weight
-    global mean
-    global sigma
     print('iteration:', iteration)
 
+    # E ステップ ========================================================================
+    # 対数尤度の期待値計算
+    # 2次元の混合正規分布を形成
+    MG = MixtureGaussian(data, mean, sigma, weight, D, cluster)
+    # モデルパラメータ値で計算される条件付き確率を算出
+    CP = (MG.T/np.sum(MG, axis=1)).T
 
-    # E step ========================================================================
-    # calculate responsibility(負担率)
-    likelihood = calc_likelihood(data, mean, sigma, weight, D)
-    gamma = (likelihood.T/np.sum(likelihood, axis=1)).T
-    N_k = [np.sum(gamma[:,d]) for d in range(D)]
+    '''
+    # M ステップ ========================================================================
+    # data[i]をすべて合計し，dataがk番目の分布から発生されたと考える確率を算出
+    CP_sum = [np.sum(CP[:,d]) for d in range(D)]
+    print(CP_sum)
 
-    # M step ========================================================================
+    # 重みの更新
+    weight =  CP_sum/N
+    '''
 
-    # caluculate weight
-    weight =  N_k/N
-
+    '''
     # calculate mean
     tmp_mu = np.zeros((D, D))
 
     for d in range(D):
         for i in range(len(data)):
-            tmp_mu[d] += gamma[i, d]*data[i]
-        tmp_mu[d] = tmp_mu[d]/N_k[d]
+            tmp_mu[d] += CP[i, d]*data[i]
+        tmp_mu[d] = tmp_mu[d]/CP_sum[d]
     mu_prev = mean.copy()
     mean = tmp_mu.copy()
 
@@ -79,17 +117,17 @@ for iteration in range(100):
         tmp_sigma[d] = np.zeros((D, D))
         for i in range(N):
             tmp = np.asanyarray(data[i]-mean[d])[:,np.newaxis]
-            tmp_sigma[d] += gamma[i, d]*np.dot(tmp, tmp.T)
-        tmp_sigma[d] = tmp_sigma[d]/N_k[d]
+            tmp_sigma[d] += CP[i, d]*np.dot(tmp, tmp.T)
+        tmp_sigma[d] = tmp_sigma[d/CP_sum[d]
 
     sigma = tmp_sigma.copy()
 
     # calculate likelihood
-    prev_likelihood = likelihood
-    likelihood = calc_likelihood(data, mean, sigma, weight, D)
+    prev_likelihood = MG
+    MG = MixtureGaussian(data, mean, sigma, weight, D, cluster)
 
     prev_sum_log_likelihood = np.sum(np.log(prev_likelihood))
-    sum_log_likelihood = np.sum(np.log(likelihood))
+    sum_log_likelihood = np.sum(np.log(MG))
     diff = prev_sum_log_likelihood - sum_log_likelihood
 
     print('sum of log likelihood:', sum_log_likelihood)
@@ -101,7 +139,7 @@ for iteration in range(100):
 
     # visualize
     for i in range(N):
-        plt.scatter(data[i,0], data[i,1], s=30, c=gamma[i], alpha=0.5, marker="+")
+        plt.scatter(data[i,0], data[i,1], s=30, c=CP[i], alpha=0.5, marker="+")
 
     for i in range(D):
         ax = plt.axes()
@@ -117,3 +155,4 @@ for iteration in range(100):
         plt.title('likelihood is converged.')
     else:
         plt.title("iter:{}".format(iteration-3))
+    '''
